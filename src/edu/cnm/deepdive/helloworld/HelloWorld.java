850a7ddc468a2ef246f9106c00248d51ccded7f5
Samuel Andazola <szola3@icloud.com>
2018-09-06 13:52:40 -0600

    New project files, with file implementing "Hello World" program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Simple class that displays the "Hello World!" message.
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello World!" to standard output.
+   * 
+   * @param args    Command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+   System.out.println("Hello World!");
+  }
+
+}
